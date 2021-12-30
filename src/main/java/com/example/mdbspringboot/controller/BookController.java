a92cd148abfe2eb470954a0e082cddc3485df381
shatha siala <shatha.siala@gmail.com>
2021-12-30 09:47:31 +0100

    removed unnecessary imports

--- src/main/java/com/example/mdbspringboot/controller/BookController.java
+++ src/main/java/com/example/mdbspringboot/controller/BookController.java
@@ -1,11 +1,7 @@
 package com.example.mdbspringboot.controller;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.ResponseEntity.BodyBuilder;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
