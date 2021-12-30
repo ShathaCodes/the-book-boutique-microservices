1e1a261f572e0a5616fdb84ab362a06e568d666d
shatha siala <shatha.siala@gmail.com>
2021-12-30 09:44:02 +0100

    removed Grocery item from microservice

--- src/main/java/com/example/mdbspringboot/repository/ItemRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.mdbspringboot.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.example.mdbspringboot.model.GroceryItem;
-
-public interface ItemRepository extends MongoRepository<GroceryItem, String> {
-}
